7e704e0bdd6957342b50006162809727235f81e4
Keala Verigan <keala@uoregon.edu>
2015-04-17 11:33:21 -0700

    Add javascripts

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -24,34 +24,96 @@ for( var i = 0; i < armNum.length; ++i) {
 
 //Req. 4-B
 
-var allArmstrongs = function() { 
-	var result = "";
+var num;
+var array = [];
+var result = "";
+var str = "";
+
+var allArmstrongs = function() {
+	for (var i = 1; i < 100000; ++i){
+
+	num = String(i);
 	
-	for (var k = 1; k <= 999999; ++k){
-		//test k 
-		if (isArmstrong(k));
-		result = result + k + ' ';
+	for (var j = 0 ; j < num.length; ++j) {
+		var arm = Math.pow(num[j], num.length);
+		array.push(arm);
+	
+	};
+	var all = 0;
+	for (var k = 0; k < array.length; ++k) {
+		all += array[k];
+
+	};
+
+	if(all == num) {
+		result += num +", ";
+		array.length = 0;
+
 	}
-        return result;
+		else {
+	
+			array.length = 0;}
 }
+
+
+	rseult = result.substring(0, result.length - 2);
+
+		return(result);
+	result = "";
+}
+
+
 
 //Req. 4-C
 
-var allSubstrings1 = function() {
-	var result ="";
+var result;
+var allSubstrings1 = function(s) {
+	s = String(s);
+	result = "";
+	for (var i = 0; i < s.length; ++i) {
+        for (var k = i + 1; k <= s.length; ++k) {
+           result += s.substring(i,k) + ",";
+        }
+        
+    }
 
-	allSubStrings1.substring(0, 1); 
+	return(result);
 }
 
-//Req. 4-D
-var allSubstrings2 = function() {
-	var result = "";
+// Req. 4-D
 
-
+var result;
+var array = [];
+var allSubstrings2 = function(s) {
+	s = String(s);
+	
+	for (var i = 0; i < s.length; ++i) {
+        for (var j = i + 1; j <= s.length; ++j) {
+           result = ""
+           result = s.substring(i,j);
+           array.push(result)
+        }
+        
+    }
+	return(array);
 }
+
 
 //Req. 4-E
 
-var maxWord = function () {
-	var result = "";
+var result;
+var maxWord = function(s) {
+	s = String(s);
+	result = s.split(" ");
+	var max = 0;
+	var word = "";
+	for (var i = 0; i < result.length; i++) {
+        if (max < result[i].length) {
+            max = result[i].length;
+            word = result[i];
+        }
+    }
+return(word);
 }
+
+
